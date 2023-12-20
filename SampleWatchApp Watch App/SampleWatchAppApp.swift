99e58c1b72c729ea36ee59e45601e6474a6f6def
Andrew Ferchland <aferchland@me.com>
2023-12-20 09:16:18 -0500

    Initial Commit

---
+++ SampleWatchApp Watch App/SampleWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWatchAppApp.swift
+//  SampleWatchApp Watch App
+//
+//  Created by Andrew J. Ferchland on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

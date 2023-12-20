09b55bf49016809faeca462057a0bfaa75bcad16
Andrew Ferchland <aferchland@me.com>
2023-12-20 16:01:58 -0500

    Update to main app providing a color gradient background.

--- SampleWatchApp Watch App/ContentView.swift
+++ SampleWatchApp Watch App/ContentView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var button = "🥺"
+    
+    struct CustomButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .padding()
+                .background(configuration.isPressed ? Color.teal : Color.mint)
+                .clipShape(Circle())
+                .foregroundColor(.mint)
+                .font(.system(size: 100, weight: .bold, design: .default))
         }
-        .padding()
     }
-}
+
+    
+    var body: some View {
+        
+            
+                VStack {
+                    
+                    Text("Tap to change mood")
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.yellow)
+                        
+                    Button(button) {
+                        if button == "🥺" {
+                            button = "😍"
+                        } else if button == "😍" {
+                            button = "🤬"
+                        } else if button == "🤬" {
+                            button = "🥺"
+                        }
+                   
+                    }
+                    
+                    .buttonStyle(CustomButtonStyle())
+                    .padding(.top)
+                    
+                    }
+                .background(LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .trailing, endPoint: .leading)
+                    .frame(width: 300.0, height: 300.0)
+                    .edgesIgnoringSafeArea(.all))
+        }
+    
+    }
 
 #Preview {
     ContentView()
